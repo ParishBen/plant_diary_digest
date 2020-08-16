32a94376d54a731e85d8f18476ac29b514122830
ParishBen <ParishBen13@gmail.com>
2020-08-15 22:54:09 -0400

    several views added; logging into account working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,14 +5,44 @@ class ApplicationController < Sinatra::Base
     enable :sessions unless test?
     set :session_secret, "secret"
   end
+ 
   get '/' do
-    "YES!"
+    erb :welcome
   end
-  get '/crazy' do
-    erb :index
+  
+  get '/login' do
+    erb :login
   end
-  get '/happy' do
-  erb :indexes
+  
+  get '/signup' do
+    erb :signup
+  end
+
+  post '/signup' do
+    user= User.new(:name=> params[:name], :username=> params[:username], :password=> params[:password])
+    if !(user.name && user.username && user.password).empty?
+      user.save
+      redirect '/login' 
+  else 
+    redirect '/failure'
+  end
+end
+  
+  get '/failure' do
+    erb :failure
+  end
+
+  post '/login' do
+    user= User.find_by(:username=> params[:username])
+    if user && user.authenticate(params[:password])
+      redirect '/account'
+    else
+      redirect '/failure'
+    end
+  end
+  
+  get '/account' do
+    "You're in your account!"
   end
 end
 #Proc.new { File.join(root, "../views/") }
