a0b176ab4d47e37602ec941540021095de9d4211
ParishBen <ParishBen13@gmail.com>
2020-08-16 17:50:48 -0400

    some views updated. breaking at plant logs

--- app/controllers/log_controller.rb
+++ app/controllers/log_controller.rb
@@ -1,6 +1,21 @@
 class LogController < ApplicationController
-    get '/plant/log' do 
-        @plant = Plant.find_by(:id=>params[:id])
-        erb :plants_log
+   
+    get '/plant/log/new' do
+        @conditions= ["Dead", "Far Worse", "Slightly Worse", "About the Same", "Slightly Livelier", "Much Healthier", "Healthiest Yet!"]
+        
+    erb :log
     end
+        get '/plant/log/:id' do 
+            @plant = Plant.find_by(:id=>params[:id])
+            erb :plants_log
+        end
+    
+        post '/plant/log/:id' do 
+            @plant = Plant.find_by(:id=>params[:id])
+            erb :plants_log
+        end
+        get '/home' do
+            erb :home
+        end
+
 end
