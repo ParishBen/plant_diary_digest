d560a4279847220ccc4fc5c4051e7c8742504585
ParishBen <ParishBen13@gmail.com>
2020-08-20 13:50:26 -0400

    added ENV-Session_Secret variable in secrets file/ added that to gitignore

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < Sinatra::Base
   configure do
     set :views, "app/views/"
     enable :sessions 
-    set :session_secret, "secret"
+    set :session_secret, ENV['SESSION_SECRET']
   end
  
   get '/' do
